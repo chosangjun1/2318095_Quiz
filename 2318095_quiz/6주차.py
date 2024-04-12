bad4b91e436c02555ba30d175eff913dc9596e09
chosangjun1 <zoonee14@naver.com>
2024-04-12 20:35:15 +0900

    Update 6주차.py

--- 2318095_quiz/6주차.py
+++ 2318095_quiz/6주차.py
@@ -1,6 +1,6 @@
-num = int(input("몇 단까지 출력할까요?:"))
-
-for num2 in range(1, num+1) :
+num = (input("몇 단까지 출력할까요?:"))
+a = int(num)
+for num2 in range(1, a+1) :
     print("------[",num2,"단]------")
-    for a in range(1,num+1) :
-      print(num2,"x",a,"=",num2*a)
+    for b in range(1,num2+1) :
+      print(num2,"x",b,"=",num2*a)
