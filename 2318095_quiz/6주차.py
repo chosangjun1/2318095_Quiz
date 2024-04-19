03f1cce1fd49e0743eba765c72fbcd9260f0b816
chosangjun1 <zoonee14@naver.com>
2024-04-19 15:14:13 +0900

    7주차
    7주차 수업

--- 2318095_quiz/6주차.py
+++ 2318095_quiz/6주차.py
@@ -1,6 +1,6 @@
 num = (input("몇 단까지 출력할까요?:"))
 a = int(num)
-for num2 in range(1, a+1) :
-    print("------[",num2,"단]------")
-    for b in range(1,num2+1) :
+for num2 in range(1, a+1):
+    print("-----[",num2,"단]------")
+    for b in range(1,num2+1):
       print(num2,"x",b,"=",num2*a)
---
+++ 2318095_quiz/함 테스트.py
@@ -0,0 +1,9 @@
+db = input("숫자를 입력하시오?:")
+
+a = int(db)
+
+for a in range(0,a+1) :
+    print("----[",a,"]단입니다.----")
+
+for b in range(0,a+1) :
+    print(a,b)
